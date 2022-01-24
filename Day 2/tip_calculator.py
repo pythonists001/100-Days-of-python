8ead441a15a0d0154433b11179d04d59516b7e02
Arvind Singh <arvind.singh@atyeti.com>
2022-01-24 21:56:58 +0530

    Added Day 3 directory

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -1,8 +1,9 @@
 print("Welcome to the tip calculator.")
 bill_amount = float(input("What was the total bill? $"))
-tip_percent = float(input("What precentage tip would you like to give? 10, 12, or 15? "))
+tip = int(input("What precentage tip would you like to give? 10, 12, or 15? "))
 num_people = int(input("How many people to split the bill? "))
 ## bill per person
-total_bill = bill_amount * ( 1 + (tip_percent/100))
+total_bill = bill_amount * ( 1 + (tip/100))
 bill_per_person = round(total_bill / num_people,2)
+bill_per_person = "{:.2f}".format(bill_per_person) ## to print values to two places after decimal
 print(f"Each person should pay : ${bill_per_person}")

c849e898fd9c2052818f53e96a7bfdf7b2a90b26
Arvind Singh <arvind.singh@atyeti.com>
2022-02-08 11:40:01 +0530

    Day 16

---
+++ Day 16 - Object Oriented Programming(OOP)/excercise1.py
@@ -0,0 +1,14 @@
+"""
+car = CarBlueprint()
+Object    Class
+"""
+from turtle import Turtle,Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()

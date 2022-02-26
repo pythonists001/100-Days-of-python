bfa77092e768df523ce3511b38b5e1509d4250c5
Arvind Singh <arvind.singh@atyeti.com>
2022-02-27 03:13:11 +0530

    Day 25

--- Day 25 - Working with CSV Data and the Pandas Library/main.py
+++ Day 25 - Working with CSV Data and the Pandas Library/main.py
@@ -23,11 +23,11 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", 
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_state = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_state.append(state)
-        new_data = pd.DataFrame(missing_state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_state.append(state)
+        new_data = pd.DataFrame(missing_states)
         
         new_data.to_csv("Day 25 - Working with CSV Data and the Pandas Library/states_to_learn.csv")
         break
